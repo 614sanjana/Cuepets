331e42c246bf80a63e30bc307b0310c294cb496a
harshitha-pai <harshithapaiofficial03@gmail.com>
2024-10-20 21:41:01 +0530

    spring boot structure

---
+++ src/main/java/com/cuepets/CuePets/CuePetsApplication.java
@@ -0,0 +1,13 @@
+package com.cuepets.CuePets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuePetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuepets/CuePets/CuePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuepets.CuePets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
