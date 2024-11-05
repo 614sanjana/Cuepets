de79e6952bc88ff33b191c65a8533ff9f47693f3
ravihitang <ravihitangofficial25@gmail.com>
2024-11-05 19:10:19 +0530

    adding getmapping

--- src/main/java/com/cuepets/CuePets/Controller/PetOwnerController.java
+++ src/main/java/com/cuepets/CuePets/Controller/PetOwnerController.java
@@ -3,9 +3,18 @@ package com.cuepets.CuePets.Controller;
 
 
 import com.cuepets.CuePets.Model.PetOwner;
+import com.cuepets.CuePets.Model.Pets;
+import com.cuepets.CuePets.Repository.PetOwnerRepo;
 import com.cuepets.CuePets.Services.PetOwnerServices;
+import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins="*")
@@ -13,13 +22,22 @@ import org.springframework.web.bind.annotation.*;
 
 public class PetOwnerController {    // check whether you can change the name as PetOwnerController
 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private PetOwnerServices petOwnerServices;
 
+    @Autowired
+    private PetOwnerRepo petOwnerRepo;
+
+
     @PostMapping(value="/addUsers")
-    private void addUsers(@RequestBody PetOwner user) {
+    public void addUsers(@RequestBody PetOwner user) {
         petOwnerServices.saveUser(user);
     }
-
+    @GetMapping(value="/getUsers")
+    public List<PetOwner> getAllStudents() {
+        return petOwnerRepo.findAll();
+    }
 
 }
