9c85a4f0ee99f87adb91ca5f24aef9ead3aebfa0
ravihitang <ravihitangofficial25@gmail.com>
2024-11-19 23:14:49 +0530

    login auth

--- src/main/java/com/cuepets/CuePets/Config/AppConfig.java
+++ src/main/java/com/cuepets/CuePets/Config/AppConfig.java
@@ -7,10 +7,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
-
     }
 }
--- src/main/java/com/cuepets/CuePets/DTO/SignIn.java
+++ src/main/java/com/cuepets/CuePets/DTO/SignIn.java
@@ -1,5 +1,8 @@
 package com.cuepets.CuePets.DTO;
 
+import lombok.Data;
+
+@Data
 public class SignIn {
     private String userPhone;
     private String userPass;
