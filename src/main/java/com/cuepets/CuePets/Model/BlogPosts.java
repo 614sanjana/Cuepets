3d839c869bc9cbd6f0006bfa8bf61bbf5104a0ff
ravihitang <ravihitangofficial25@gmail.com>
2024-12-05 23:33:12 +0530

    final commits

--- src/main/java/com/cuepets/CuePets/Model/BlogPosts.java
+++ src/main/java/com/cuepets/CuePets/Model/BlogPosts.java
@@ -1,16 +1,4 @@
 package com.cuepets.CuePets.Model;
-
-<<<<<<< HEAD
-import org.springframework.data.annotation.Id;
-
-public class BlogPosts {
-    @Id
-    private String blogId;
-
-    private String authorName;
-    private String ownerId;
-    
-=======
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -25,5 +13,4 @@ public class BlogPosts {
     private String blogTitle;
     private String blogDesc;
     private String blogContent;
->>>>>>> da73c111566697e05defba21ed64ec49fe5d71b4
 }
